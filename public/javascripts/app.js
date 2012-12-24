4c6a5459c55f2bf6227be53d04592fd6470676c6
niraj swami <fivesquared@gmail.com>
2012-12-24 13:07:07 -0600

    JSK!

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -36,3 +36,41 @@
   }
 
 })(jQuery, this);
+
+
+function getXHR(){
+	var ret;
+	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+		ret=new XMLHttpRequest();
+	} else {// code for IE6, IE5
+		ret=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	return ret;
+}
+
+// LOGIN FUNCTIONALITY //
+function __trackMetric(email, behavior, scope, value){
+	//
+	var xmlhttp = getXHR();
+	xmlhttp.onreadystatechange = function(){
+		if (xmlhttp.readyState==4 && xmlhttp.status==200){
+			//
+			var respStr = xmlhttp.responseText + "";
+			if (respStr == 'error'){
+				// no-op
+				alert('tracking error!');
+			}else{
+				// reset stuff..
+				resetUI();
+			}
+    	}
+	}
+	xmlhttp.open("POST","http://contextexperiment.orchestra.io/ns_trackbehavior.php",true);
+	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	xmlhttp.send("e="+email+"&b="+behavior+"&s="+scope+"&v="+value);
+	//
+}
+
+function resetUI(){
+	$('#behavior').val("");
+}
---
+++ public/ns_include.php
@@ -0,0 +1,13 @@
+﻿<?php
+	//
+	session_start();
+	// db plug
+	mysql_connect(
+  		$server = 'a.db.shared.orchestra.io',
+  		$username = 'user_cc2975f3',
+  		$password = 'UMN2^EIYAE(rnc'
+	);
+	//
+	mysql_select_db('db_cc2975f3');
+	//
+?>
