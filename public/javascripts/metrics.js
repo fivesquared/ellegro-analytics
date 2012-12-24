c19d19b5b7a0a8db0a896f3ee885f2a947a950c0
niraj swami <fivesquared@gmail.com>
2012-12-24 13:15:14 -0600

    JSK!

---
+++ public/javascripts/metrics.js
@@ -0,0 +1,76 @@
+;(function ($, window, undefined) {
+  'use strict';
+
+  var $doc = $(document),
+      Modernizr = window.Modernizr;
+
+  $(document).ready(function() {
+    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
+    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
+    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
+    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
+    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
+    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
+    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
+    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
+    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
+    $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
+    $.fn.foundationClearing         ? $doc.foundationClearing() : null;
+
+    $.fn.placeholder                ? $('input, textarea').placeholder() : null;
+  });
+
+  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
+  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
+  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
+  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+
+  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
+  if (Modernizr.touch && !window.location.hash) {
+    $(window).load(function () {
+      setTimeout(function () {
+        window.scrollTo(0, 1);
+      }, 0);
+    });
+  }
+
+})(jQuery, this);
+
+
+function getXHR(){
+	var ret;
+	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+		ret=new XMLHttpRequest();
+	} else {// code for IE6, IE5
+		ret=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	return ret;
+}
+
+// LOGIN FUNCTIONALITY //
+function __trackMetric(email, behavior, scope, value){
+	//
+	var xmlhttp = getXHR();
+	xmlhttp.onreadystatechange = function(){
+		if (xmlhttp.readyState==4 && xmlhttp.status==200){
+			//
+			var respStr = xmlhttp.responseText + "";
+			if (respStr == 'error'){
+				// no-op
+				alert('tracking error!');
+			}else{
+				// reset stuff..
+				resetUI();
+			}
+    	}
+	}
+	xmlhttp.open("POST","http://contextexperiment.orchestra.io/ns_trackbehavior.php",true);
+	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	xmlhttp.send("e="+email+"&b="+behavior+"&s="+scope+"&v="+value);
+	//
+}
+
+function resetUI(){
+	$('#behavior').val("");
+}
