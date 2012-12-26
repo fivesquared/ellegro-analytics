adb5a23ee6133202cd06d5acda0b6096695f552e
niraj swami <fivesquared@gmail.com>
2012-12-26 12:16:22 -0600

    JSK!

--- public/experiment.php
+++ public/experiment.php
@@ -1,3 +1,39 @@
+<?php
+	//
+	include "ns_include.php";
+	// we must insert our new project accordingly
+	$table = "courserandomizer";
+	//
+	// let's get the latest course
+	$query = "SELECT latestrequestcourse FROM ".$table." WHERE id=1";
+	//
+	$result = mysql_query($query);
+	//
+	$latestCourse = "";
+	//
+	while ($row = mysql_fetch_array($result)){
+		$latestCourse = $row['latestrequestcourse'];
+	}
+	if ($latestCourse == "CourseA"){
+		$latestCourse = "CourseB";
+	}else{
+		$latestCourse = "CourseA";
+	}
+	// update the system and the time stamp
+	$query = "UPDATE ".$table." SET latestrequestcourse='$latestCourse' WHERE id = 1";
+	$result = mysql_query($query);
+	//
+	$numRows = mysql_affected_rows();
+	if ($numRows == 1){
+		// we have a success
+	}else{
+		// we have a failures
+		$latestCourse = "CourseA";
+	}
+	//
+	mysql_close();
+	//
+?>
 <!DOCTYPE html>
 
 <!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
@@ -33,24 +69,13 @@
 <body style="background: url(images/background.jpg) no-repeat center center fixed; 	-webkit-background-size: cover;	-moz-background-size: cover; -o-background-size: cover; background-size: cover;">>
 
   <div class="row">
-	<div class="six columns">
-		<div style="padding:25px; background:rgba(0,0,0,0.25); color:#f1f1f1; font-size:1.25em;">
-			<h3 style="color:#f1f1f1;">we need your help!</h3>
-			<span class="label">it should only take a few minutes, really.</span><br/><br/>
-			<p>We've put together a simple presentation about information and how, in today's world, we need to be little cautious when sharing it. We want to understand what makes the presentation worthy of the viewer's time and interest.</p>
-			<p>If you could share a few minutes of your time and some basic feedback, we'd appreciate it tremendously!</p><p><strong>Register via the link to the right</strong>, and we'll get you through it quick! Hopefully, you'll also pick up a couple of interesting bits from the presentation.</p>
-			<span class="label">thank you!</span>
+	<div class="twelve columns">
+		<div class="panel radius">
+			<h3>welcome, john doe</h3>
+			<div align="center"><a href="<?php echo $latestCourse; ?>.html"><img src="images/course_titler.png"></a></div>
+			<br/><br/>
 		</div>
 	</div>
-	<div class="six columns">
-		<img src="images/title.png">
-		<div class="panel radius">
-			<h3>start here</h3>
-			<input type="text" placeholder="your email">
-			<input type="text" placeholder="your name">
-			<a class="small radius button" href="experiment.php">begin experiment</a>
-		</div>
-    </div>
   </div>
 
   
