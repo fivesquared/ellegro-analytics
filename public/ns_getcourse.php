c5f392161d829229118edc3ba8329d7627f5c5a2
niraj swami <fivesquared@gmail.com>
2012-12-26 11:31:48 -0600

    JSK!

---
+++ public/ns_getcourse.php
@@ -0,0 +1,38 @@
+﻿<?php
+	//
+	include "ns_include.php";
+	// we must insert our new project accordingly
+	$table = "courserandomizer";
+	//
+	// let's get the latest course
+	$query = "SELECT latestrequestcourse FROM ".$table." WHERE id=1";
+	//
+	$result = mysql_query($query);
+	//
+	$latestCourse = "";
+	//
+	while ($row = mysql_fetch_array($result)){
+		$latestCourse = $row['latestrequestcourse'];
+	}
+	if ($latestCourse == "CourseA"){
+		$latestCourse = "CourseB";
+	}else{
+		$latestCourse = "CourseA";
+	}
+	// update the system and the time stamp
+	$query = "UPDATE ".$table." SET latestrequestcourse='$latestCourse' WHERE id = 1";
+	$result = mysql_query($query);
+	//
+	$numRows = mysql_affected_rows();
+	if ($numRows == 1){
+		// we have a success
+		echo	$latestCourse;
+	}else{
+		// we have a failures
+		echo	"-1: error!";
+	}
+	//
+	mysql_close();
+	//
+
+?>
